4bfa85bccf9d074da0c9705185b091f8e59ba1cd
guricerin <chanbo1e9@gmail.com>
2022-03-19 21:49:34 +0900

    add log to search_handler

--- backend/server/search_handler.go
+++ backend/server/search_handler.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"errors"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,13 +17,14 @@ func (s *Server) getSearch() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		signinUser, err := s.fetchUserFromSession(c)
 		if err != nil {
-			log.Printf("getSearch() 0: %s\n", err.Error())
-			c.JSON(http.StatusBadRequest, errorRes(errors.New("bad request")))
+			s.Warn(c, http.StatusUnauthorized, nil, err)
+			c.JSON(http.StatusUnauthorized, errorRes(errors.New("unauthorized")))
 			return
 		}
 
 		var req searchReq
 		if err := c.BindJSON(&req); err != nil {
+			s.Warn(c, http.StatusBadRequest, nil, err)
 			c.JSON(http.StatusBadRequest, errorRes(err))
 			return
 		}
@@ -33,6 +33,7 @@ func (s *Server) getSearch() gin.HandlerFunc {
 		case "user_id":
 			users, err := s.userStore.Search(req.Keyword, UserIdSearch)
 			if err != nil {
+				s.Error(c, http.StatusInternalServerError, &signinUser, err)
 				c.JSON(http.StatusInternalServerError, errorRes(errors.New("server error")))
 				return
 			}
@@ -40,6 +41,8 @@ func (s *Server) getSearch() gin.HandlerFunc {
 			for _, u := range users {
 				usersJson = append(usersJson, userRes(u))
 			}
+
+			s.Info(c, http.StatusOK, &signinUser, "success to search user_id")
 			c.JSON(http.StatusOK, gin.H{
 				"users": usersJson,
 			})
@@ -47,6 +50,7 @@ func (s *Server) getSearch() gin.HandlerFunc {
 		case "user_name":
 			users, err := s.userStore.Search(req.Keyword, UserNameSearch)
 			if err != nil {
+				s.Error(c, http.StatusInternalServerError, &signinUser, err)
 				c.JSON(http.StatusInternalServerError, errorRes(errors.New("server error")))
 				return
 			}
@@ -54,6 +58,8 @@ func (s *Server) getSearch() gin.HandlerFunc {
 			for _, u := range users {
 				usersJson = append(usersJson, userRes(u))
 			}
+
+			s.Info(c, http.StatusOK, &signinUser, "success to search user_name")
 			c.JSON(http.StatusOK, gin.H{
 				"users": usersJson,
 			})
@@ -61,6 +67,7 @@ func (s *Server) getSearch() gin.HandlerFunc {
 		case "grumble":
 			grumbles, err := s.grumbleStore.Search(signinUser.Id, req.Keyword)
 			if err != nil {
+				s.Error(c, http.StatusInternalServerError, &signinUser, err)
 				c.JSON(http.StatusInternalServerError, errorRes(errors.New("server error")))
 				return
 			}
@@ -70,11 +77,14 @@ func (s *Server) getSearch() gin.HandlerFunc {
 			for _, g := range grumbles {
 				grumblesJson = append(grumblesJson, grumbleRes(g))
 			}
+
+			s.Info(c, http.StatusOK, &signinUser, "success to search grumble")
 			c.JSON(http.StatusOK, gin.H{
 				"grumbles": grumblesJson,
 			})
 			return
 		default:
+			s.Warn(c, http.StatusBadRequest, &signinUser, err)
 			c.JSON(http.StatusBadRequest, errorRes(errors.New("不正な検索種別が指定されています。")))
 			return
 		}
